30bdaec7b48e4ab7b65e26bef77bb0a48df45cc7
Paulo Tiago <paulotiagocm@hotmail.com>
2017-01-05 14:25:38 +0000

    first componnent

---
+++ resources/assets/js/components/headerportifolio.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom affix-top">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-bar-top">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#page-top">Paulo Mariano</a>
+            </div>
+    
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="nav-bar-top">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden active">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#badges">Conquistas</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#projects">Projetos</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
