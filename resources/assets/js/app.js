7eacd71b15f7e0177b841cda20c1e8843dc28935
Paulo Tiago <paulotiagocm@hotmail.com>
2017-01-06 18:01:38 +0000

    using template on archive

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,7 +18,7 @@ Vue.component('fivestars', require('./components/fivestars.vue'));
 Vue.component('presentation', require('./components/presentation.vue'));
 Vue.component('badges', require('./components/badges.vue'));
 Vue.component('projectsitem', {
-    template: '<div class="col-sm-4 col-lg-5 col-lg-offset-1 projects-item"><div class="imgcontainer"><img class="img-responsive" src="img/portifolio-laravel-vue.png" alt="img de projeto guia"></img></div><div class="informacoes"><p><span class="titles">Titulo:</span> {{title}}</p><p><span class="titles">Link:</span> <a style="color: white;" href="http://www.testworks.com.br/portifolio">http://www.testworks.com.br/portifolio</a></p><p><span class="titles">Descrição:</span> Projeto para estudos de ambiente laravel e divulgação dos projetos que participei</p><p><span class="titles">Tags:</span> PHP7, MySql, Bootstrap (Sass), Cloud9, Vuejs, Laravel</p><p>Data: {{quando}}</p></div></div>',
+    template: require('./components/projectsitem.vue'),
     data: function () {
       return {
         title: 'Portifolio em laravel + vue',
--- resources/assets/js/components/projectsitem.vue
+++ resources/assets/js/components/projectsitem.vue
@@ -4,11 +4,11 @@
             <img class="img-responsive" src="img/portifolio-laravel-vue.png" alt="img de projeto guia"></img>
         </div>
         <div class="informacoes">
-            <p><span class="titles">Titulo:</span> Portifolio em laravel + vue</p>
+            <p><span class="titles">Titulo:</span> {{title}}</p>
             <p><span class="titles">Link:</span> <a style="color: white;" href="http://www.testworks.com.br/portifolio">http://www.testworks.com.br/portifolio</a></p>
             <p><span class="titles">Descrição:</span> Projeto para estudos de ambiente laravel e divulgação dos projetos que participei</p>
             <p><span class="titles">Tags:</span> PHP7, MySql, Bootstrap (Sass), Cloud9, Vuejs, Laravel</p>
-            <p>Data: 01/2017</p>
+            <p>Data: {{quando}}</p>
         </div>
     </div>
 </template>
