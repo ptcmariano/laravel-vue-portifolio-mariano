94e44adf0f6b66b6f70eed2731f5d40a44e6066c
Paulo Tiago <paulotiagocm@hotmail.com>
2017-01-06 17:57:01 +0000

    component project item with parameters

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,15 @@ Vue.component('headerportifolio', require('./components/headerportifolio.vue'));
 Vue.component('fivestars', require('./components/fivestars.vue'));
 Vue.component('presentation', require('./components/presentation.vue'));
 Vue.component('badges', require('./components/badges.vue'));
+Vue.component('projectsitem', {
+    template: '<div class="col-sm-4 col-lg-5 col-lg-offset-1 projects-item"><div class="imgcontainer"><img class="img-responsive" src="img/portifolio-laravel-vue.png" alt="img de projeto guia"></img></div><div class="informacoes"><p><span class="titles">Titulo:</span> {{title}}</p><p><span class="titles">Link:</span> <a style="color: white;" href="http://www.testworks.com.br/portifolio">http://www.testworks.com.br/portifolio</a></p><p><span class="titles">Descrição:</span> Projeto para estudos de ambiente laravel e divulgação dos projetos que participei</p><p><span class="titles">Tags:</span> PHP7, MySql, Bootstrap (Sass), Cloud9, Vuejs, Laravel</p><p>Data: {{quando}}</p></div></div>',
+    data: function () {
+      return {
+        title: 'Portifolio em laravel + vue',
+        quando: '02/2017'
+      }
+    }
+});
 Vue.component('projects', require('./components/projects.vue'));
 Vue.component('contact', require('./components/contact.vue'));
 
---
+++ resources/assets/js/components/projectsitem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="col-sm-4 col-lg-5 col-lg-offset-1 projects-item">
+        <div class="imgcontainer">
+            <img class="img-responsive" src="img/portifolio-laravel-vue.png" alt="img de projeto guia"></img>
+        </div>
+        <div class="informacoes">
+            <p><span class="titles">Titulo:</span> Portifolio em laravel + vue</p>
+            <p><span class="titles">Link:</span> <a style="color: white;" href="http://www.testworks.com.br/portifolio">http://www.testworks.com.br/portifolio</a></p>
+            <p><span class="titles">Descrição:</span> Projeto para estudos de ambiente laravel e divulgação dos projetos que participei</p>
+            <p><span class="titles">Tags:</span> PHP7, MySql, Bootstrap (Sass), Cloud9, Vuejs, Laravel</p>
+            <p>Data: 01/2017</p>
+        </div>
+    </div>
+</template>
